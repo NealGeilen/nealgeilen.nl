7f4312957771e59dc384f345e720d96aecd7da3d
Neal Geilen <info@nealgeilen.nl>
2022-07-02 20:50:27 +0200

    Trying own style

---
+++ src/components/Card.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="card-container">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card'
+}
+</script>
+
+<style scoped>
+.card-container{
+  border-radius: 4px;
+  padding: 5px 6px;
+  background-color: #000;
+  color: #fff
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,19 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <Card>
+    <ParticlesComponent id="jkawhdlkjawhdkjawh"/>
+  </Card>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import Card from '@/components/Card.vue'
+import { ParticlesComponent } from 'particles.vue3'
 
 @Options({
   components: {
+    ParticlesComponent,
+    Card,
     HelloWorld
   }
 })
