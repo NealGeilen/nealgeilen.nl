e93a7e0f9d92d4dd6123a7d45deff86ac19d5917
Neal Geilen <info@nealgeilen.nl>
2022-07-19 10:34:37 +0200

    Setup Vue and Vuetify V2

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,18 @@
 <template>
-  <Card>
-    <ParticlesComponent id="jkawhdlkjawhdkjawh"/>
-  </Card>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component'
-import HelloWorld from '@/components/HelloWorld.vue'
-import Card from '@/components/Card.vue'
-import { ParticlesComponent } from 'particles.vue3'
+import Vue from 'vue'
+import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
 
-@Options({
+export default Vue.extend({
+  name: 'Home',
   components: {
-    ParticlesComponent,
-    Card,
     HelloWorld
   }
 })
-export default class Home extends Vue {}
 </script>
